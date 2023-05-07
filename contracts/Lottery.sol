c2be33c188dbfecc22a15aa5b2e9727827918775
wigya <amajewski505@gmail.com>
2023-05-07 14:21:18 +0200

    Lottery-app

---
+++ contracts/Lottery.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.2 <0.9.0;
+
+
+contract Lottery {
+    address public owner;
+    address payable[] public players;
+    uint public lotteryId;
+    mapping(uint => address payable) public lotteryHistory;
+
+    constructor() {
+        owner = msg.sender;
+        lotteryId = 1;
+    }
+
+    function getWinnerByLottery(uint _lotteryId) public view returns(address payable) {
+        return lotteryHistory[_lotteryId];
+    }
+
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+
+    function getPlayers() public view returns(address payable[] memory) {
+        return players;
+    }
+
+    function enter() public payable {
+        require(msg.value > .01 ether);
+
+        // address of player entering lotery
+        players.push(payable(msg.sender));
+    }
+
+    function getRandomNumber() public view returns(uint) {
+        return uint(keccak256(abi.encodePacked(owner, block.timestamp)));
+    }
+
+    function pickWinner() public onlyOwner {
+        uint index = getRandomNumber() % players.length;
+        players[index].transfer(address(this).balance);
+
+        lotteryHistory[lotteryId] = players[index];
+        lotteryId++;
+
+        // reset the state of the contract
+        players = new address payable[](0);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+}
